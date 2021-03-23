43d223ee3b9bb8dbad6b18defbe84fe1a2ebbf45
pBudz <78623024+pBudz@users.noreply.github.com>
2021-03-22 21:36:07 -0500

    Update RocketFC.ino

--- RocketFC/RocketFC.ino
+++ RocketFC/RocketFC.ino
@@ -1,67 +1,77 @@
-#include <InternalTemperature.h>
+///#include <I2C.h>
+//#include <TeensyThreads.h>
+//#include <String.h>
+//#include <Wire.h>
+//#include <SPI.h>
+//#include <InternalTemperature.h>
+
+
+//------------------------------------------
+//CORE libraries for hardware interface
 #include <Adafruit_Sensor.h>
 #include <ArduinoJson.h>
-///#include <I2C.h>
-#include <Adafruit_MLX90393.h>
-#include <TeensyThreads.h>
-#include <String.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BME280.h>
 #include <SD.h>
-#include <Adafruit_GPS.h>
+//------------------------------------------
+//------------------------------------------
+//BME Library & hardware interface definitions for BME280, atmospheric sensor
+#include <Adafruit_BME280.h>
 #define BME_SCK 13
 #define BME_MISO 12
 #define BME_MOSI 11
 #define BME_CS 10
+#define ALTITUDE_inHg (29.96*33.864)//check nearest airfield or weather station for local curent air pressure.
+//------------------------------------------
+//------------------------------------------
+//BNO055 & math logic library for IMU's.
+#include <Adafruit_BNO055.h>
+//#include <utility/imumaths.h>
+//------------------------------------------
+//------------------------------------------
+//GPS Libraries and definitions
 #define GPSSerial Serial1
 #define GPSECHO false
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-
+#include <Adafruit_GPS.h>
 Adafruit_GPS GPS(&GPSSerial);
+//------------------------------------------
 uint32_t timer = millis();
-int redLED = 3;
-int greenLED = 4;
-int yellowLED = 5;
-int testLED = 8;
-int count = 0;
+
 const int buzzer = 24;
 
 float humidity;
 float pressure;
 float temperature;
 
-#define ALTITUDE_inHg (30.07*33.864)//check nearest airfield or weather station for local curent air pressure.
 uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
 const int chipSelect = BUILTIN_SDCARD;
+//------------------------------------------
+//declares BME object
 Adafruit_BME280 bme;
+//instantiates BNO055
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+//------------------------------------------
 unsigned long delayTime;
 double launchzeroalt;
 File myFile;
 
 //todo
-StaticJsonDocument<35000> bme280;
-StaticJsonDocument<225000> totallist;
-StaticJsonDocument<35000> acceler;
-StaticJsonDocument<35000> orientation;
-StaticJsonDocument<35000> linearacc;
-StaticJsonDocument<35000> magnetometer;
-StaticJsonDocument<35000> angvelocity;
-
+//Declare Json Object, add names of different lists to be sent to SD file.
+StaticJsonDocument<430000> totallist;
+JsonArray atmo = totallist.createNestedArray("atmo");
+JsonArray accel2 = totallist.createNestedArray("accel2");
+JsonArray orient = totallist.createNestedArray("orient");
+JsonArray linear = totallist.createNestedArray("linear");
+JsonArray magnet = totallist.createNestedArray("magnet");
+JsonArray angvel = totallist.createNestedArray("angvel");
 
 int correcttime;
 
 void setup() {
-  bme280["atmospheric sensors"] = "atmo";
-  acceler["acceleration"] = "accel";
-  orientation["orientation"] = "orien";
-  linearacc["Linear accel"] = "linear";
-  magnetometer["magnetometer"] = "mag";
-  angvelocity["angular velocity"] = "angvel";
+  //------------------------------------------
+  //instantiate
+
+  //------------------------------------------
   pinMode(buzzer, OUTPUT);
-  
+
   Serial.begin(9600);
   Serial.begin(115200);
   Serial.println("Orientation Sensor Test"); Serial.println("");
@@ -70,7 +80,7 @@ void setup() {
   if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    Serial.print("no BNO055 detected");
     while (1);
   }
   Serial.println("Complete!");
@@ -78,12 +88,10 @@ void setup() {
   delay(1000);
   bno.setExtCrystalUse(true);
 
-  while (!Serial) {
-    digitalWrite(greenLED, LOW);
-    digitalWrite(redLED, LOW);
-    digitalWrite(yellowLED, LOW);
+ // while (!Serial) {
+
     // wait for serial port to connect.
-  }
+  //}
   Serial.print("Initializing SD card...");//Preps SD card for writing
 
   if (!SD.begin(chipSelect)) {
@@ -92,11 +100,10 @@ void setup() {
   }
   Serial.println("initialization done.");
   while (!Serial);
-  myFile = SD.open("t.txt", FILE_WRITE);
-  
+  myFile = SD.open("t.txt", FILE_WRITE);//change field 1 to whatever you want to name the file.. "example.txt"
+
   // 9600 baud is the default rate for the Ultimate GPS
   GPSSerial.begin(9600);
-
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PGCMD_ANTENNA);
@@ -105,29 +112,20 @@ void setup() {
 
   unsigned status;
   status = bme.begin();
-  pinMode(redLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(testLED, OUTPUT);
-  if (status) {
-    digitalWrite(yellowLED, LOW);
-  }
+  //if (status) {
+
+ // }
 
   launchzeroalt = (bme.readPressure() / 100.0F);
   correcttime = millis();
 }
-JsonArray total = totallist.createNestedArray("total");
-JsonArray atmo = bme280.createNestedArray("atmo");
-JsonArray accel2 = acceler.createNestedArray("accel2");
-JsonArray orient = orientation.createNestedArray("orient");
-JsonArray linear = linearacc.createNestedArray("linear");
-JsonArray magnet = magnetometer.createNestedArray("magnet");
-JsonArray angvel = angvelocity.createNestedArray("angvel");
+
 
 
 void loop() {
   timer = millis() - correcttime;
-  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;// declare variables to assign values to from the bno055
+  
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
   bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
   bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
@@ -136,23 +134,23 @@ void loop() {
   bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
 
   int8_t boardTemp = bno.getTemp();
-//  Serial.println();
-//  Serial.print(F("temperature: "));
- // Serial.println(boardTemp);
+  //  Serial.println();
+  //  Serial.print(F("temperature: "));
+  // Serial.println(boardTemp);
 
   uint8_t system, gyro, accel, mag = 0;
   bno.getCalibration(&system, &gyro, &accel, &mag);
-//  Serial.println();
-//  Serial.print("Calibration: Sys=");
-//  Serial.print(system);
-//  Serial.print(" Gyro=");
-//  Serial.print(gyro);
-//  Serial.print(" Accel=");
-//  Serial.print(accel);
-//  Serial.print(" Mag=");
-//  Serial.println(mag);
+  //  Serial.println();
+  //  Serial.print("Calibration: Sys=");
+  //  Serial.print(system);
+  //  Serial.print(" Gyro=");
+  //  Serial.print(gyro);
+  //  Serial.print(" Accel=");
+  //  Serial.print(accel);
+  //  Serial.print(" Mag=");
+  //  Serial.println(mag);
 
-//  Serial.println("--");
+  //  Serial.println("--");
   delay(BNO055_SAMPLERATE_DELAY_MS);
   digitalWrite(buzzer, LOW);
   atmo.add((bme.readPressure() / 100.0F) * 0.03);
@@ -181,25 +179,19 @@ void loop() {
   accel2.add(timer);
 
   if (myFile) {                         //code to write to microssd
-    if (millis() - correcttime >= 1000) {
-      
-      total.add(atmo);
-      total.add(accel2);
-      total.add(orient);
-      total.add(linear);
-      total.add(magnet);
-      total.add(angvel);
+    if (millis() - correcttime >= 100000) {
+
       Serial.println("Writing to test.txt...");
       serializeJsonPretty(totallist, myFile);
       myFile.close();
       Serial.println("done.");
 
-      while (millis() - correcttime >= 1000){
+      while (millis() - correcttime >= 1000) {
         //tone(buzzer, 5000);
       }
     }
   }
-  
+
 
 }
 
@@ -207,9 +199,9 @@ double printEventAccelerometerX(sensors_event_t* event) {
   double x = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ACCELEROMETER) {
     x = event->acceleration.x;
-   // Serial.print(x);
+    // Serial.print(x);
     return x;
-}
+  }
 }
 double printEventAccelerometerY(sensors_event_t* event) {
   double y = -1000000; //dumb values, easy to spot problem
@@ -217,110 +209,110 @@ double printEventAccelerometerY(sensors_event_t* event) {
     y = event->acceleration.y;
     //Serial.print(y);
     return y;
-}
+  }
 }
 double printEventAccelerometerZ(sensors_event_t* event) {
   double z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ACCELEROMETER) {
     z = event->acceleration.z;
-   // Serial.print(z);
+    // Serial.print(z);
     return z;
-}
+  }
 }
 double printEventOrientationX(sensors_event_t* event) {
   double x = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ORIENTATION) {
     x = event->orientation.x;
-  //  Serial.print(x);
+    //  Serial.print(x);
     return x;
-}
+  }
 }
 double printEventOrientationY(sensors_event_t* event) {
   double y = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ORIENTATION) {
     y = event->orientation.y;
-  //  Serial.print(y);
+    //  Serial.print(y);
     return y;
-}
+  }
 }
 double printEventOrientationZ(sensors_event_t* event) {
   double z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ORIENTATION) {
     z = event->orientation.z;
-  //  Serial.print(z);
+    //  Serial.print(z);
     return z;
-}
+  }
 }
 double printEventLinearAccelerometerX(sensors_event_t* event) {
   double x = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
     x = event->acceleration.x;
-   // Serial.print(x);
+    // Serial.print(x);
     return x;
-}
+  }
 }
 double printEventLinearAccelerometerY(sensors_event_t* event) {
   double y = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
     y = event->acceleration.y;
-  //  Serial.print(y);
+    //  Serial.print(y);
     return y;
-}
+  }
 }
 double printEventLinearAccelerometerZ(sensors_event_t* event) {
   double z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
     z = event->acceleration.z;
-   // Serial.print(z);
+    // Serial.print(z);
     return z;
+  }
 }
-}
-double printEventMagneticX(sensors_event_t* event){
+double printEventMagneticX(sensors_event_t* event) {
   double x = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
     x = event->magnetic.x;
-   // Serial.print(x);
-    return x;   
+    // Serial.print(x);
+    return x;
+  }
 }
-}
-double printEventMagneticY(sensors_event_t* event){
+double printEventMagneticY(sensors_event_t* event) {
   double y = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
     y = event->magnetic.y;
-   // Serial.print(y);
-    return y;   
+    // Serial.print(y);
+    return y;
+  }
 }
-}
-double printEventMagneticZ(sensors_event_t* event){
+double printEventMagneticZ(sensors_event_t* event) {
   double z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
     z = event->magnetic.z;
-   // Serial.print(z);
-    return z;   
-}
+    // Serial.print(z);
+    return z;
+  }
 }
 
-double printRotationVectorX(sensors_event_t* event){
+double printRotationVectorX(sensors_event_t* event) {
   double x = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
     x = event->magnetic.x;
-  //  Serial.print(x);
-    return x;   
+    //  Serial.print(x);
+    return x;
+  }
 }
-}
-double printRotationVectorY(sensors_event_t* event){
+double printRotationVectorY(sensors_event_t* event) {
   double y = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
     y = event->magnetic.y;
-   // Serial.print(y);
-    return y;   
+    // Serial.print(y);
+    return y;
+  }
 }
-}
-double printRotationVectorZ(sensors_event_t* event){
+double printRotationVectorZ(sensors_event_t* event) {
   double z = -1000000; //dumb values, easy to spot problem
   if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
     z = event->magnetic.z;
-  //  Serial.print(z);
-    return z;   
-}
+    //  Serial.print(z);
+    return z;
+  }
 }
